7e240542d50fe7cfe06e1e030ef7ef504d6e403b
danmesfin <danielmsfn@gmail.com>
2023-01-09 14:51:43 +0300

    fixed index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="flex justify-center">
         <div
           className="flex flex-col m-4 justify-center bg-transparent 
         opacity-80 bg-gradient-to-br from-black via-red-500 to-black text-xl rounded-sm px-8"
